65da5aecf83c3707abc0a5bbf5425d2c9fe7554c
thebearingedge <contact@timdav.is>
2017-06-23 21:29:10 -0700

    Refactor register page. #6

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,6 +1,13 @@
+import React from 'react'
 import withRedux from 'next-redux-wrapper'
 
 import { initStore } from '../core'
+import { Login } from '../layouts'
 import { Register, mapDispatch } from '../registration/register'
 
-export default withRedux(initStore(), null, mapDispatch)(Register)
+const Page = props =>
+  <Login title='Register'>
+    <Register { ...props }/>
+  </Login>
+
+export default withRedux(initStore(), null, mapDispatch)(Page)
