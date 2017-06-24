bfb5d773cb51e2e6a9a225470d19a0eb72506ffe
thebearingedge <contact@timdav.is>
2017-06-23 21:29:49 -0700

    Add flag to prevent Link prefetches during tests. #6

--- client/test/shared.js
+++ client/test/shared.js
@@ -2,6 +2,8 @@ import { stub } from 'sinon'
 import sinonChai from 'sinon-chai'
 import chai, { expect } from 'chai'
 
+process.env.NODE_ENV = 'test'
+
 chai.use(sinonChai)
 
 export { expect, stub }
