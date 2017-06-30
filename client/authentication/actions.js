899e4f16458b4df2e61301ac1128fc1f32e0a2db
thebearingedge <contact@timdav.is>
2017-06-29 20:28:46 -0700

    Define an action for clearing form submission errors. #29

--- client/authentication/actions.js
+++ client/authentication/actions.js
@@ -1,5 +1,12 @@
+import * as types from './action-types'
+
+export const signinSubmitted = () => ({
+  type: types.SIGNIN_SUBMITTED
+})
+
 export const onSubmit = credentials =>
   async (dispatch, getState, { api }) => {
+    dispatch(signinSubmitted())
     const { data } = await api.post('/authenticate', credentials)
     return data
   }
--- client/authentication/actions.unit.test.js
+++ client/authentication/actions.unit.test.js
@@ -1,7 +1,7 @@
 import { describe, beforeEach, afterEach, it } from 'mocha'
 import { injectStore, expect, stub } from '../test/unit'
 import { api } from '../core'
-import { onSubmit } from './actions'
+import * as actions from './actions'
 
 describe('authentication/actions', () => {
 
@@ -15,15 +15,21 @@ describe('authentication/actions', () => {
 
     beforeEach(() => stub(api, 'post'))
 
-    afterEach(() => api.post.restore())
+    afterEach(() => {
+      api.post.restore()
+      store.clearActions()
+    })
 
     it('posts a set of user credentials', async () => {
       const credentials = { username: 'foo', password: 'bar' }
       api.post
         .withArgs('/authenticate', credentials)
         .resolves({ data: { user: { username: 'foo' }, token: 'bar' } })
-      const result = await store.dispatch(onSubmit(credentials))
+      const result = await store.dispatch(actions.onSubmit(credentials))
       expect(result).to.deep.equal({ user: { username: 'foo' }, token: 'bar' })
+      expect(store.getActions()).to.deep.equal([
+        actions.signinSubmitted()
+      ])
     })
 
   })
