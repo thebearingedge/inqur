dbe3ac404efb72f57f4b8e035c8b67e08aed4d52
thebearingedge <contact@timdav.is>
2017-06-30 22:56:38 -0700

    Enhance integration test to verify display of submission errors. #29

--- client/authentication/signin.int.test.js
+++ client/authentication/signin.int.test.js
@@ -4,16 +4,19 @@ import moxios from 'moxios'
 import Router from 'next/router'
 import { withStore, expect, mount, stub } from '../test/integration'
 import { api } from '../core'
+import { FormError } from '../components'
 import Signin from './signin'
 
 describe('authentication/signin', () => {
 
-  const connect = withStore({ api, Router })
-  const Connected = connect(Signin)()
+  let store
   let wrapper
 
   beforeEach(() => {
-    wrapper = mount(<Connected/>)
+    const provide = withStore({ api, Router })
+    const { WithStore, store: _store } = provide(Signin)()
+    store = _store
+    wrapper = mount(<WithStore/>)
     moxios.install(api)
     stub(Router, 'push')
   })
@@ -56,6 +59,31 @@ describe('authentication/signin', () => {
     moxios.stubOnce('POST', '/authenticate', {
       status: 201,
       response: { user: { username: 'foo' }, token: 'token' }
+    })
+    wrapper
+      .find('[name="username"]')
+      .first()
+      .simulate('change', { target: { value: 'foo' } })
+    wrapper
+      .find('[name="password"]')
+      .first()
+      .simulate('change', { target: { value: 'foo' } })
+    wrapper.find('[type="submit"]').simulate('submit')
+  })
+
+  it.only('displays authentication failure errors', done => {
+    moxios.stubOnce('POST', '/authenticate', {
+      status: 401,
+      response: { error: 'Invalid Login' }
+    })
+    store.subscribe(() => {
+      const { authentication: { error } } = store.getState()
+      if (!error) return
+      setTimeout(() => {
+        const formError = wrapper.find(FormError).first()
+        expect(formError).to.have.text('foo')
+        done()
+      })
     })
     wrapper
       .find('[name="username"]')
