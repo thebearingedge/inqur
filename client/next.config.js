5f86e57bfceabc91ceb74c37add593eb3c9a1111
thebearingedge <contact@timdav.is>
2017-07-08 22:45:23 -0700

    Fixed error in next config. #35

--- client/next.config.js
+++ client/next.config.js
@@ -4,9 +4,9 @@ const { DefinePlugin } = require('webpack')
 module.exports = {
   webpack: (config, { dev }) => {
     config.plugins = config.plugins
-      .filter(plugin => dev
-        ? plugin.constructor.name !== 'UglifyJsPlugin'
-        : true
+      .filter(plugin =>
+        process.env.NODE_ENV === 'production' ||
+        plugin.constructor.name !== 'UglifyJsPlugin'
       )
       .concat(new DefinePlugin({
         'process.env.API_URL': JSON.stringify(process.env.API_URL),
