d50b2c517612db07f91c66130878dc4e7fbb59a8
thebearingedge <contact@timdav.is>
2017-07-09 20:05:43 -0700

    Simplify end-to-end tests. #48

--- e2e/signin.e2e.test.js
+++ e2e/signin.e2e.test.js
@@ -1,23 +1,24 @@
-import { describe, it } from 'mocha'
-import { fakeUser, browser, baseUrl } from './__setup__'
+import { describe, beforeEach, it } from 'mocha'
+import bcrypt from 'bcrypt'
+import { fakeUser, browser, baseUrl, knex } from './__setup__'
 
 describe('/signin', () => {
 
-  const { username, email, password } = fakeUser()
+  const { username, email, password: unhashed } = fakeUser()
+
+  beforeEach(async () => {
+    const salt = await bcrypt.genSalt(10)
+    const password = await bcrypt.hash(unhashed, salt)
+    await knex
+      .insert({ username, email, password })
+      .into('users')
+  })
 
   it('lets a registered user sign in', async () => {
     await browser
-      .goto(`${baseUrl}/register`)
-      .type('[name="username"]', username)
-      .type('[name="email"]', email)
-      .type('[name="password"]', password)
-      .type('[name="retypePassword"]', password)
-      .click('[type="submit"]')
-      .wait('h1')
-    await browser
       .goto(`${baseUrl}/signin`)
       .type('[name="username"]', username)
-      .type('[name="password"]', password)
+      .type('[name="password"]', unhashed)
       .click('[type="submit"]')
       .wait('h1')
   })
